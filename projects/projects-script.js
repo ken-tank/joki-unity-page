8a7c393b74b5f1ba88a352052ed10927858ba29a
KenTank <ulilalkhaq4@gmail.com>
2025-04-09 13:57:36 +0700

    update style

--- projects/projects-script.js
+++ projects/projects-script.js
@@ -14,11 +14,22 @@ function ShowPage(id)
     .then(res => res.json())
     .then(json => {
         json[id].forEach(element => {
+            var dom = document.createElement("div");
             var item = itemHTML;
+            var platforms = element["platform"].sort();
             var rendered = item.replace(/{{\s*(\w+)\s*}}/g, (_, key) => {
                 return element[key] !== undefined ? element[key] : '';
             });
-            project_parent.innerHTML += rendered;
+            dom.innerHTML = rendered;
+            project_parent.appendChild(dom);
+            var platform_item = dom.querySelector("#platform-item");
+            var parent = platform_item.parentElement;
+            platforms.forEach(value => {
+                var new_item = platform_item.cloneNode(true);
+                new_item.innerHTML = value.trim();
+                parent.appendChild(new_item);
+            });
+            platform_item.classList += " hidden";
         });
     });
 }
