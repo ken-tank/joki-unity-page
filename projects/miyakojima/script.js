4962243e1397f66a45a66f89cc2f352ccd599695
KenTank <ulilalkhaq4@gmail.com>
2025-05-12 16:15:45 +0700

    Update script.js

--- projects/miyakojima/script.js
+++ projects/miyakojima/script.js
@@ -1,6 +1,6 @@
 function Unlock() 
 {
     var key = document.getElementById("download_key").value;
-    var link = "Td+Cx1+bIOm21v615em5L5qI58t2qDBo/led3C7Hu1sgNZ5WYVq68ZFBfQ0JNDlQDPEHB4sylUuocZo0OhI6jlvQ9YGQztgHb/+harSkTAUa/Xu2/9dUXdt8r9ERfuen";
+    var link = "Td+Cx1+bIOm21v615em5L5qI58t2qDBo/led3C7Hu1t/o8+VhLPqR7ISUslcKL0K75G2YlEJXSpR3iXiiOKvJvGANPnrPdAn8/WoJ/1NU+5fDy6Yv1USDXwJH3FjOInW";
     Downloader.Download(key, link);
 }
