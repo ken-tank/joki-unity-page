afb48202015a4b265b3b36badad466044c1e1b27
KenTank <ulilalkhaq4@gmail.com>
2025-06-09 09:51:01 +0700

    add Web Interactive Megalitikum

---
+++ projects/web-interactive-megalitikum/script.js
@@ -0,0 +1,6 @@
+function Unlock() 
+{
+    var key = document.getElementById("download_key").value;
+    var link = "KRo1hGNmvSVkFPC+kL4YeJ1MzjdraAWLkwyIaDye5gnNlo2Z99U66kQ+f5+lnOm+ZBoqCKM3HaKnmumSyUBxC5Ge0oQVKzcMQ+zmi0qZy1zdh589IvhaGiBsgv2SZ4wd";
+    Downloader.Download(key, link);
+}
