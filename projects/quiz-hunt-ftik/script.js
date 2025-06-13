6d2e633bd2223e60050841658eb40d223e035f8a
KenTank <ulilalkhaq4@gmail.com>
2025-06-13 19:00:30 +0700

    add quiz-hunt-ftik

---
+++ projects/quiz-hunt-ftik/script.js
@@ -0,0 +1,6 @@
+function Unlock() 
+{
+    var key = document.getElementById("download_key").value;
+    var link = "n17BfhS7S+3ofgNKrWbRNXb32+QAbNqPnzz9jFLRHc2EBsabv8MEZFZfEIJNu1LBnFAdL5ZBwkg65Fe39/FZt2tzdPl5TjzFlWZZl/TJ26obz1zaOYAx6SpbWqxjGOXd";
+    Downloader.Download(key, link);
+}
