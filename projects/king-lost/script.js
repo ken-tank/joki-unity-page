efdbddc6e08d128cc32d8200f85b6d07829a2d92
KenTank <ulilalkhaq4@gmail.com>
2025-06-18 18:26:12 +0700

    add page King Lost

---
+++ projects/king-lost/script.js
@@ -0,0 +1,6 @@
+function Unlock() 
+{
+    var key = document.getElementById("download_key").value;
+    var link = "4xm99ea/8Y/HB/XqNTmT+oFv8sYcAjEmYQe7YTczfHAmGGWU5jiAkj1hFTY1eUqAxuexnQj5ka0N53hPMZHMNJvpyLkCiCl7E9UQNRpwKT67U2zC++SQGtEVpADAVRX7";
+    Downloader.Download(key, link);
+}
