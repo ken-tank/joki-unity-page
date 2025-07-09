bfa69166fa68ff268b45d6f19a3b7560a21708fa
KenTank <ulilalkhaq4@gmail.com>
2025-07-09 09:07:24 +0700

    add page and modif some

--- projects/downloader.js
+++ projects/downloader.js
@@ -5,10 +5,17 @@ class Downloader {
         await EncryptorSHA256.decrypt(value, key.trim(), "unity").then(value => {
             decrypted = value;
         });
+        var msg = document.getElementById("download_msg");
         if (decrypted == "")
         {
-            alert("Key Invalid!")
+            alert("Key Invalid!");
+            msg.innerHTML = "Key Invalid or Wrong!";
+            msg.hidden = false;
             return;
+        }
+        else
+        {
+            msg.hidden = true;
         }
         
         window.open(decrypted, '_blank');
--- projects/projects-script.js
+++ projects/projects-script.js
@@ -1,35 +1,70 @@
-function ShowPage(id)
+var data;
+var splitted;
+var itemHTML;
+var currentIndexPage = 999;
+
+function splitArrayIntoChunks(array, chunkSize) {
+    const result = [];
+    for (let i = 0; i < array.length; i += chunkSize) {
+        result.push(array.slice(i, i + chunkSize));
+    }
+    return result;
+}
+
+function ShowPage(index)
 {
+    if (currentIndexPage == index) return;
+
     var project_parent = document.getElementById("project-list");
     project_parent.innerHTML = "";
 
-    var itemHTML = "";
+    splitted[index].forEach(element => {
+        var dom = document.createElement("div");
+        var item = itemHTML;
+        var platforms = element["platform"].sort();
+        var rendered = item.replace(/{{\s*(\w+)\s*}}/g, (_, key) => {
+            return element[key] !== undefined ? element[key] : '';
+        });
+        dom.innerHTML = rendered;
+        project_parent.appendChild(dom);
+        var platform_item = dom.querySelector("#platform-item");
+        var parent = platform_item.parentElement;
+        platforms.forEach(value => {
+            var new_item = platform_item.cloneNode(true);
+            new_item.innerHTML = value.trim();
+            parent.appendChild(new_item);
+        });
+        platform_item.classList += " hidden";
+    });
+    currentIndexPage = index;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var pageRoot = document.getElementById("page");
+    var pageItem = pageRoot.children[0];
+
     fetch("/joki-unity-page/Components/project_list_item.html")
     .then(res => res.text())
     .then(html => {
         itemHTML = html;
     });
-
     fetch("/joki-unity-page/projects/list-project.json")
     .then(res => res.json())
     .then(json => {
-        json[id].forEach(element => {
-            var dom = document.createElement("div");
-            var item = itemHTML;
-            var platforms = element["platform"].sort();
-            var rendered = item.replace(/{{\s*(\w+)\s*}}/g, (_, key) => {
-                return element[key] !== undefined ? element[key] : '';
+        data = json;
+        splitted = splitArrayIntoChunks(data, 5);
+        
+        for (let index = 0; index < splitted.length; index++) {
+            var text = pageItem.innerHTML.replace("{index}", index + 1);
+            var cloned = pageItem.cloneNode(true);
+            cloned.innerHTML = text;
+            cloned.addEventListener("click", callback => {
+                ShowPage(index);
             });
-            dom.innerHTML = rendered;
-            project_parent.appendChild(dom);
-            var platform_item = dom.querySelector("#platform-item");
-            var parent = platform_item.parentElement;
-            platforms.forEach(value => {
-                var new_item = platform_item.cloneNode(true);
-                new_item.innerHTML = value.trim();
-                parent.appendChild(new_item);
-            });
-            platform_item.classList += " hidden";
-        });
+            pageRoot.appendChild(cloned);
+        }
+        pageItem.hidden = true;
+        
+        ShowPage(0);
     });
-}
+})
